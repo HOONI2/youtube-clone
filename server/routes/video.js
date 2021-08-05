387b7fad7664524e2a525ced259e8dadb3c27530
unknown <singersman@naver.com>
2021-08-05 20:01:12 +0900

    thumbnail

--- server/routes/video.js
+++ server/routes/video.js
@@ -5,6 +5,8 @@ const { auth } = require("../middleware/auth");
 const multer = require("multer");
 const path = require("path");
 
+var ffmpeg = require("fluent-ffmpeg");
+
 // STORAGE MULTER CONFIG
 let storage = multer.diskStorage({
   destination: (req, file, cb) => {
@@ -33,7 +35,6 @@ const upload = multer({
 router.post("/uploadfiles", (req, res) => {
   upload(req, res, (err) => {
     if (err) {
-      console.log(err);
       return res.json({ success: false, err });
     }
     return res.json({
@@ -42,6 +43,49 @@ router.post("/uploadfiles", (req, res) => {
       fileName: res.req.file.filename,
     });
   });
+});
+
+router.post("/thumbnail", (req, res) => {
+  // 썸네일 생성하고 비디오 러닝타임도 가져오기
+
+  let filePath = "";
+  let fileDuration = "";
+
+  ffmpeg.setFfmpegPath("C:\\dev\\ffmpeg\\bin");
+
+  // 비디오 정보 가져오기
+  ffmpeg.ffprobe(req.body.url, function (err, metadata) {
+    console.log(metadata);
+    console.log(metadata.format.duration);
+    fileDuration = metadata.format.duration;
+  });
+
+  // 썸네일 생성
+  ffmpeg(req.body.url)
+    .on("filenames", function (filenames) {
+      console.log("Will generate " + filenames.join(", "));
+      console.log(filenames);
+
+      filepath = "uploads/thumbnails/" + fielnames[0];
+    })
+    .on("end", function () {
+      console.log("Screentshots taken");
+      return res.json({
+        success: true,
+        url: filePath,
+        fileDuration: fileDuration,
+      });
+    })
+    .on("error", function (err) {
+      console.log(err);
+      return res.json({ success: false, err });
+    })
+    .screenshots({
+      count: 3,
+      folder: "uploads/thumbnails",
+      size: "320x240",
+      filename: "thumbnail-%b.png",
+    });
 });
 
 module.exports = router;
